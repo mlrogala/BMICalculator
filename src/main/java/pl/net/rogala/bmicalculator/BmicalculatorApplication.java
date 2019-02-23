4b593a81a047f5fcc821dd724ae49db843275d14
Marlena Rogala <marlenalukomska@wp.pl>
2019-02-23 18:26:28 +0100

    first commit

---
+++ src/main/java/pl/net/rogala/bmicalculator/BmicalculatorApplication.java
@@ -0,0 +1,13 @@
+package pl.net.rogala.bmicalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmicalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmicalculatorApplication.class, args);
+    }
+
+}
